5e9b4472db101dbf5e16e96db6df0dbbcfecbaba
Hide-Koba <37530094+Hide-Koba@users.noreply.github.com>
2023-04-11 23:26:45 +1000

    tmp commit

--- chat-app/src/App.js
+++ chat-app/src/App.js
@@ -1,23 +1,45 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+
+  const [chatLogs, setChatLog] = useState([]); // [ {name: 'John', content: 'Hello'}, {name: 'Mary', content: 'Hi'} ]
+  const [chatContent, setChatContent] = useState('');
+
+  const handleChatContent = (event) => {
+    setChatContent(event.target.value);
+  };
+
+  const handleSubmit = (event) => {
+    console.log('handling');
+    console.log('log: '+chatContent);
+    setChatLog(chatLogs=>[...chatLogs,{name: 'User', content: chatContent, time: new Date().toLocaleTimeString(), role: 'user'}]);
+    setChatContent('');
+
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div id="chatLog" style={{height:"85vh", border:"1px solid #ccc"}}>
+        <ul>
+          {chatLogs.map((chat, index) => (
+            <li>
+              <div>
+                <div className="name">{chat.name}</div>
+                <div className="content">{chat.content}</div>
+                <div className="time">{chat.time}</div>
+              </div>
+            </li>
+          ))}
+        </ul>
+      </div>
+      <div id="ChatInput">
+        <textarea id="chatInput" onChange={handleChatContent} value={chatContent} />
+
+        <button id="chatButton" onClick={handleSubmit}>Send</button>
+      </div>
+        
     </div>
   );
 }
